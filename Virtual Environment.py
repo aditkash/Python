51a2805f54c3d34da6305181439af7e5f2ca5744
aditkash <aditya.kashikar@twconsulting.in>
2021-11-09 13:11:01 +0530

    init

---
+++ Virtual Environment.py
@@ -0,0 +1,6 @@
+# What virtual environment does?
+# Virtual Environment saves the current state of our compiler along with the state of their modules and libraries. 
+# So in this way even if Python has made certain changes in its module, our virtual environment can still work as before even after years. 
+# We can also install different packages and “dataframes” in our virtual environment.
+# To be more clear, the virtual environment works exactly the same way as the Python we have installed on our windows/mac/Linux currently 
+# because a virtual environment is just a clone of the original product.
